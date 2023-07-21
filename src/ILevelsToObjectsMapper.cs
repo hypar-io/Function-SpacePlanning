2407e01c008ac4120db1d94819a129c105de84f5
srudenkoamc <srudenko@amcbridge.com>
2023-07-21 13:07:50 +0300

    Create ILevelToObjectsMapper interface

---
+++ src/ILevelsToObjectsMapper.cs
@@ -0,0 +1,10 @@
+﻿using Elements;
+using System.Collections.Generic;
+
+namespace SpacePlanning
+{
+    internal interface ILevelsToObjectsMapper
+    {
+        public bool TryMapWallToLevels(Wall wall, Dictionary<string, List<Wall>> wallsByLevel);
+    }
+}
